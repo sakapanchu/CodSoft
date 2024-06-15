a95d14b1de1602912c70ecf00d47a290fd5bfa4e
sakaniya panchalingam <117504870+sakapanchu@users.noreply.github.com>
2024-06-15 23:17:01 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,83 @@
+from tkinter import *
+from tkinter.ttk import Combobox
+from tkinter import messagebox
+import string, random
+
+root = Tk()
+root.geometry("500x500")
+root.title("Password Generator")
+root.config(bg="beige")
+root.resizable(False, False)
+
+def password_generate():
+    try:
+        length_password = solidboss.get()
+        small_letters = string.ascii_lowercase
+        capital_letters = string.ascii_uppercase
+        digits = string.digits
+        special_character = string.punctuation
+        all_list = []
+        all_list.extend(list(small_letters))
+        all_list.extend(list(capital_letters))
+        all_list.extend(list(digits))
+        all_list.extend(list(special_character))
+        random.shuffle(all_list)
+        password.set("".join(all_list[0:length_password]))
+    except:
+        messagebox.askretrycancel("A Problem Has Been Occured", "Please Try Again")
+
+all_no = {"1": "1", "2": "2", "3": "3", "4": "4", "5": "5",
+    "6": "6", "7": "7", "8": "8", "9": "9", "10": "10",
+    "11": "11", "12": "12", "13": "13", "14": "14", "15": "15",
+    "16": "16", "17": "17", "18": "18", "19": "19", "20": "20",
+    "21": "21", "22": "22", "23": "23", "24": "24", "25": "25",
+    "26": "26", "27": "27", "28": "28", "29": "29", "30": "30",
+    "31": "31", "32": "32", "33": "33", "34": "34", "35": "35",
+    "36": "36", "37": "37", "38": "38", "39": "39", "40": "40",
+    "41": "41", "42": "42", "43": "43", "44": "44", "45": "45",
+    "46": "46", "47": "47", "48": "48", "49": "49", "50": "50",
+    "51": "51", "52": "52", "53": "53", "54": "54", "55": "55",
+    "56": "56", "57": "57", "58": "58", "59": "59", "60": "60",
+    "61": "61", "62": "62", "63": "63", "64": "64", "65": "65",
+    "66": "66", "67": "67", "68": "68", "69": "69", "70": "70",
+    "71": "71", "72": "72", "73": "73", "74": "74", "75": "75",
+    "76": "76", "77": "77", "78": "78", "79": "79", "80": "80",
+    "81": "81", "82": "82", "83": "83", "84": "84", "85": "85",
+    "86": "86", "87": "87", "88": "88", "89": "89", "90": "90",
+    "91": "91", "92": "92", "93": "93", "94": "94", "95": "95",
+    "96": "96", "97": "97", "98": "98", "99": "99", "100": "100"
+}
+
+Title = Label(root, text="Password Generator", bg="beige", fg="black", font=("futura", 15, "bold"))
+Title.pack(anchor="center", pady="20px")
+
+length = Label(root, text="Select the Length of Your Password :- ", fg="darkgreen", bg="beige", font=("ubuntu", 12))
+length.place(x="20px", y="70px")
+
+def on_enter(e):
+    generate_btn['bg'] = "grey"
+    generate_btn['fg'] = "white"
+
+def on_leave(e):
+    generate_btn['bg'] = "orange"
+    generate_btn['fg'] = "black"
+
+solidboss = IntVar()
+Selector = Combobox(root, textvariable=solidboss, state="readonly")
+Selector['values'] = [l for l in all_no.keys()]
+Selector.current(7)
+Selector.place(x="240px", y="72px")
+
+generate_btn = Button(root, text="Generate Password", bg="orange", fg="black", font=("ubuntu", 15), cursor="hand2", command=password_generate)
+generate_btn.bind("<Enter>", on_enter)
+generate_btn.bind("<Leave>", on_leave)
+generate_btn.pack(anchor="center", pady="50px")
+
+result_lable = Label(root, text="Generated Password Here :- ", bg="beige", fg="darkgreen", font=("ubuntu", 12))
+result_lable.place(x="20px", y="160px")
+
+password = StringVar()
+password_final = Entry(root, textvariable= password, state="readonly", fg="blue", font=("ubuntu", 15))
+password_final.place(x="200px", y="160px")
+
+root.mainloop()
